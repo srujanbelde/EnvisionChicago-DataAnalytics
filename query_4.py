fe2e70030cc914602cddf0b4b3eb4ce6432dd6a3
Pranay Kumar Rasulury <pranay.rasulury1@gmail.com>
2018-04-16 20:46:13 -0500

    updates

--- query_4.py
+++ query_4.py
@@ -3,35 +3,6 @@ from utils import jaccard, address_refine
 import csv
 
 restaurant_data = {}
-# with open('reviews_60601-60606.csv', newline='') as rev:
-#     r = csv.reader(rev, delimiter=',')
-#     for row in r:
-#         if row[0] == 'date':
-#             continue
-#         rating = row[4].strip()
-#         num = 4
-#         while not rating.isdigit() and num <= len(row):
-#             rating = row[num-1].strip()
-#             num += 1
-#         try:
-#             rating = float(rating)
-#         except ValueError:
-#             print(rating)
-#         restaurant_id = row[-1].strip()
-#         try:
-#             restaurant_data[restaurant_id].append(rating)
-#         except KeyError:
-#             restaurant_data[restaurant_id] = []
-#             restaurant_data[restaurant_id].append(rating)
-#         # print("(" + str(i) + ")" + str(rating) + "  <----->  " + restaurant_id)
-
-# for key, val in restaurant_data.items():
-#     sum = 0
-#     for x in val:
-#         sum += x
-#     avg = sum/len(val)
-#     restaurant_data[key] = []
-#     restaurant_data[key].append(avg)
 
 with open('restaurants_60601-60606.csv', newline='', encoding='utf8') as rev:
     r = csv.reader(rev, delimiter=',')
